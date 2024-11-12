23679f3584178481bda6a2691d5a3b8fe0e59301
Egesa Michael <praisewebhosts@gmail.com>
2024-11-12 19:19:23 +0300

    updated api

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -1,8 +1,7 @@
 "use client";
 import dynamic from "next/dynamic";
 import React from "react";
-import ChartOne from "../Charts/ChartOne";
-import ChartTwo from "../Charts/ChartTwo";
+
 import ChatCard from "../Chat/ChatCard";
 
 import CardDataStats from "../CardDataStats";
@@ -11,9 +10,6 @@ const MapOne = dynamic(() => import("@/components/Maps/MapOne"), {
   ssr: false,
 });
 
-const ChartThree = dynamic(() => import("@/components/Charts/ChartThree"), {
-  ssr: false,
-});
 
 const ECommerce: React.FC = () => {
   return (
@@ -106,9 +102,7 @@ const ECommerce: React.FC = () => {
       </div>
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <ChartOne />
-        <ChartTwo />
-        <ChartThree />
+    
         <MapOne />
         <div className="col-span-12 xl:col-span-8">
       
